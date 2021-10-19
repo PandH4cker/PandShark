dd2310f82c5d5a7eb94946f2ce8baff3cf7e08f6
MrrRaph <dray.raph@gmail.com>
2021-10-19 18:57:35 +0200

    Added DHCP

--- src/protocols/dhcp/DHCP.java
+++ src/protocols/dhcp/DHCP.java
@@ -3,8 +3,16 @@ package protocols.dhcp;
 import core.headers.layer2.Layer2Protocol;
 import core.headers.layer3.Layer3Protocol;
 import protocols.PcapPacketData;
+import protocols.arp.HardwareType;
 
 public class DHCP extends PcapPacketData {
+    DHCPMessageType messageType;
+    HardwareType hardwareType;
+    Integer hardwareAddressLength;
+    Integer hops;
+    String transactionID;
+    Integer secondsElapsed;
+
 
 
     public DHCP(Integer id, Long sequenceNumber, Layer2Protocol layer2Protocol, Layer3Protocol layer3Protocol) {
